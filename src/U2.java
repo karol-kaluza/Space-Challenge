3333f4cc99f8267fdb75b45a2600be5a3d71836a
Karol Kaluza <karol.kaluza@gmail.com>
2020-11-15 23:10:48 +0100

    classes

--- src/U2.java
+++ src/U2.java
@@ -1,2 +1,11 @@
 public class U2 extends Rocket {
+    @Override
+    public boolean launch(int cargoCarried, int cargoLimit, int percentExplosionChance) {
+        return super.launch(cargoCarried, cargoLimit, percentExplosionChance);
+    }
+
+    @Override
+    public boolean land(int cargoCarried, int cargoLimit, int percentExplosionChance) {
+        return super.land(cargoCarried, cargoLimit, percentExplosionChance);
+    }
 }
