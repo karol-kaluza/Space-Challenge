52a1814ae5342499f5d614455234963700f44862
Karol Kaluza <karol.kaluza@gmail.com>
2020-11-16 18:23:04 +0100

    ArrayList of items

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,6 +1,22 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Simulation {
-    ArrayList <String> loadItems(){
-        return
+
+    private String filePhase1 = "phase-1.txt";
+    private String filePhase2 = "phase-2.txt";
+
+
+    ArrayList<Item> items = new ArrayList<>();
+
+    private ArrayList <Item> loadItems(Scanner file) {
+        ArrayList <Item> items = new ArrayList<>();
+        while (file.hasNextLine()){
+            String [] separatedStrings = file.nextLine().split("=");
+            String itmName = separatedStrings[0];
+            int itWeight = (Integer.parseInt(separatedStrings[1]));
+            items.add(new Item(itmName,itWeight));
+        }
+        return items;
+    }
 }
